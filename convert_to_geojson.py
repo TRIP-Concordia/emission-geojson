ac77e1a59c3600a4ccba34f848a6bf12bfa59d25
K Fitzsimmons <kfitzsimmons@gmail.com>
2019-05-21 12:50:44 -0400

    Formatted converted using python/black

--- convert_to_geojson.py
+++ convert_to_geojson.py
@@ -6,42 +6,51 @@ from pathlib import Path
 
 
 @click.command()
-@click.option('-f', '--file', 'filepath',
-              required=True,
-              help='Converts e-mission .json dump file to .geojson')
+@click.option(
+    "-f",
+    "--file",
+    "filepath",
+    required=True,
+    help="Converts e-mission .json dump file to .geojson",
+)
 def convert(filepath):
     emission_data = None
-    with open(filepath, 'r') as emission_f:
+    with open(filepath, "r") as emission_f:
         emission_data = json.loads(emission_f.read())
 
-
-    geojson_data = {
-        'type': 'FeatureCollection',
-        'features': []
-    }
-    point_cols = ['accuracy', 'altitude', 'bearing', 'elapsedRealtimeNanos',
-                  'filter', 'fmt_time', 'latitude', 'longitude', 'sensed_speed', 'ts']
+    geojson_data = {"type": "FeatureCollection", "features": []}
+    point_cols = [
+        "accuracy",
+        "altitude",
+        "bearing",
+        "elapsedRealtimeNanos",
+        "filter",
+        "fmt_time",
+        "latitude",
+        "longitude",
+        "sensed_speed",
+        "ts",
+    ]
     for coordinate in emission_data:
-        data = coordinate['data']
-        if 'latitude' in data and 'longitude' in data:
+        data = coordinate["data"]
+        if "latitude" in data and "longitude" in data:
             point = {
-                'type': 'Feature',
-                'geometry': {
-                    'type': 'Point',
-                    'coordinates': [data['longitude'], data['latitude']]
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [data["longitude"], data["latitude"]],
                 },
-                'properties': {}
+                "properties": {},
             }
             for col in point_cols:
-                point['properties'][col] = data[col]
-            geojson_data['features'].append(point)
-
+                point["properties"][col] = data[col]
+            geojson_data["features"].append(point)
 
     path = Path(filepath).resolve()
-    out_fn = '{parent}/{fn}.geojson'.format(parent=path.parent, fn=path.stem)
-    with open(out_fn, 'w') as geojson_f:
+    out_fn = "{parent}/{fn}.geojson".format(parent=path.parent, fn=path.stem)
+    with open(out_fn, "w") as geojson_f:
         geojson_f.write(json.dumps(geojson_data))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     convert()
